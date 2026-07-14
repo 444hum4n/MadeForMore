ddf9661490d9bdc92d3f774d0eb338592c85ab5d
444hum4n <444hum4n@gmail.com>
2026-07-14 13:36:41 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "perfect-day-v1";
+const CACHE = "perfect-day-v2";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
